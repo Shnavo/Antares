9fde20d439ba19e538ad1db51ce0c1e9141c14f2
Shnavo <szango297@gmail.com>
2026-05-17 21:39:34 +0200

    feat(main.py): Created connection with database

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import psycopg2
+from datetime import datetime
+from creds.db import postgres as p
+
 if __name__ == "__main__":
-    print("hello world")
+    try:
+        connection = psycopg2.connect(
+            host=p.host,
+            port=p.port,
+            database=p.database,
+            user=p.user,
+            password=p.password,
+        )
+        cursor = connection.cursor()
+        query = "INSERT INTO users (username, password, email_address, created_at) VALUES (%s, %s, %s, %s);"
+        data = ("test", "pass", "test@test.com", datetime.now())
+        cursor.execute(query, data)
+        cursor.execute("SELECT * FROM users;")
+        connection.commit()
+        result = cursor.fetchall()
+        print(result)
+    except Exception as e:
+        print(e)
