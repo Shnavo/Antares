3bac2260bec966949ca9b7ac22ff80ea8b76c99e
Shnavo <szango297@gmail.com>
2026-06-06 22:03:21 +0200

    Followed tutorial on FastAPI up until the section on "Request Body".

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,11 +8,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
 
 
 # Order matters
@@ -54,7 +50,79 @@ async def get_model(model_name: ModelName):
     #     case ModelName.lenet:
     #         return {"model_name": model_name}
     if model_name == ModelName.alexnet:
-        return {"model_name": model_name}
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-    if model_name.value == "resnet":
-        return {"model_name": model_name}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing item that has a long description"})
+    return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
+
+
+if __name__ == "__main__":
+    import requests
+
+    url = "http://127.0.0.1:8000"
+    headers = {"Content-Type": "application/json"}
+    data = {"name": 42, "price": 42.0, "description": "this is a good item"}
+    r = requests.post(f"{url}/items/", headers=headers, json=data)
+    print(r.raise_for_status)
+
+    get = requests.get("https://www.w3schools.com/python")
+    # print(get.json())
